9d3e1244595c4f5948d2c0241b88ab0b21377c9f
anfeng8379 <anfeng_8379@163.com>
2016-12-05 15:41:22 +0800

    MVC-测试
    添加MVC测试程序

---
+++ MvcApplication1/MvcApplication1/Controllers/TestController.cs
@@ -0,0 +1,43 @@
+﻿using MvcApplication1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcApplication1.Controllers
+{
+    public class TestController : Controller
+    {
+        //
+        // GET: /Test/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [AllowAnonymous]
+        public ActionResult Test_Save()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        [ValidateAntiForgeryToken]
+        public ActionResult Test_Save(Register_Model model, string returnUrl)
+        {
+            //return RedirectToAction("Index");
+            return View("aaa");
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        [ValidateAntiForgeryToken]
+        public ActionResult abc(Register_Model model, string returnUrl)
+        {
+            return View("Test_Save");
+        }
+    }
+}
---
+++ MvcApplication1/MvcApplication1/Models/TestModels.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MvcApplication1.Models
+{
+    public class TestModels
+    {
+    }
+
+    public class Register_Model
+    {
+        [Required]
+        [Display(Name = "用户名")]
+        public string UserName1 { get; set; }
+
+        [Required]
+        [StringLength(100, ErrorMessage = "{0} 必须至少包含 {2} 个字符。", MinimumLength = 6)]
+        [DataType(DataType.Password)]
+        [Display(Name = "密码")]
+        public string Password1 { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "确认密码")]
+        [Compare("Password", ErrorMessage = "密码和确认密码不匹配。")]
+        public string ConfirmPassword1 { get; set; }
+    }
+}
